f11e9cc31c99862b8c3ebffe4cbc86df33189e35
backyes <backyes@gmail.com>
2020-12-11 14:37:18 +0000

    Refactor DIR

--- predict/demo.py
+++ predict/demo.py
@@ -23,3 +23,16 @@ if __name__ == '__main__':
     print("---------------------------------------")
     for key in result:
         print(result[key][1])
+
+    im = Image.open(os.path.dirname(__file__) + "/test/img/result.png")
+    img = np.array(im.convert('RGB'))
+    t = time.time()
+    '''
+    result,img,angel分别对应-识别结果，图像的数组，文字旋转角度
+    '''
+    result, img, angle = model.model(
+        img, model='keras', adjust=True, detectAngle=True)
+    print("It takes time:{}s".format(time.time() - t))
+    print("---------------------------------------")
+    for key in result:
+        print(result[key][1])
